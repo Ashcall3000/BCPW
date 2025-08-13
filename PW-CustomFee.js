41b4bd35ee856b585f158c484e62860607417817
Ashcall3000 <Ashcall3000@gmail.com>
2025-08-12 17:23:26 -0700

    Create PW-CustomFee.js
    This is a script that will allow users in accela to see the fee amount that is associated with the unit and quantity.

---
+++ PW-CustomFee.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name         PW-CustomFee
+// @namespace    http://tampermonkey.net/
+// @version      0.0.1
+// @description  Used for Accela to show Fee Unit costs per quantity amount
+// @author       Christopher Sullivan
+// @match        https://butteco-test-av.accela.com/*
+// @require      https://github.com/Ashcall3000/BCPW/raw/refs/heads/main/Searcher.js
+// @require      https://github.com/Ashcall3000/BCPW/raw/refs/heads/main/ThreadController.js
+// @require      https://github.com/Ashcall3000/BCPW/raw/refs/heads/main/CookieController.js
+// @downloadURL  https://github.com/Ashcall3000/BCPW/raw/refs/heads/main/PW-CustomFee.js
+// @updateURL    https://github.com/Ashcall3000/BCPW/raw/refs/heads/main/PW-CustomFee.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+    var Threads = new ThreadController("CustomFees");
+    
+    Threads.add('inner-window', function() {
+        if (!exists('input', {id:'fee-*'})) {
+            let feeRows = search('tr', {element:'#AccelaMainTable', id:'row*', all:true});
+            for (let i = 0; i < feeRows.length; i++) {
+                let td = search('td', {element:feeRows[i], all:true})[3];
+                let input = addTag(td, 'input', {
+                    id: 'fee-' + i,
+                    eclass: 'portlet-form-input-field',
+                    attr: 'title',
+                    value: 'FeeCost',
+                    style: 'height: 19px; width: 4em; background-color:#f5f6f5'
+                });
+                addAttribute(input, 'type', 'text');
+                input.addEventListener('change', EventChange);
+                input.value = '$' + search('input', {attr:'name', value:'*FormulaName,' + i + ')'}).value;
+                input.disabled = true;
+            }
+        }
+    });
+    
+    function EventChange() {
+        let feeCosts = search('input', {id:'fee-*', all:true});
+        for (let i = 0; i < feeCosts.length; i++) {
+            if (feeCosts[i].value != '') {
+                let num = feeCosts[i].getAttribute('id').substring(4);
+                search('input', {attr:'name', value:'*FormulaName,' + num + ')'}).value = feeCosts[i].value;
+            }
+        }
+    }
+})();
